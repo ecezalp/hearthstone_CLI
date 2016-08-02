513ba81448155c0e5c3c635877b482895ce9dacd
Ece Ozalp <github email address>
2016-08-02 18:26:40 -0400

    next commit

--- API.rb
+++ API.rb
@@ -1,30 +1,61 @@
-require_relative "wrapper.rb"
+require "json"
+require "pry"
+require "rest-client"
+
 
 class User
-  attr_reader :card_array, :class_choice
+  attr_reader :card_array, :class_choice, :class_card_array
   attr_accessor :user_cards
   
   def initialize
     all_cards = RestClient.get('https://api.hearthstonejson.com/v1/13807/enUS/cards.collectible.json')
     @card_array = JSON.parse(all_cards)
     @user_cards = []
+    @class_card_array = []
   end
 
-  def get_class_cards(class_choice)
-    @card_array.collect do |card_element|
-      if card_element["playerClass"].downcase == class_choice.downcase || card_element["playerClass"].downcase == 'neutral'
-      @user_cards << card_element
-      end
+  def get_class_cards(user_class_input)
+    
+    hero_variable = ""
+
+    if user_class_input == "1"
+       hero_variable = "mage"
+    elsif user_class_input == "2"
+       hero_variable = "paladin"
+    elsif user_class_input == "3"
+       hero_variable = "druid"
+    elsif user_class_input == "4"
+       hero_variable = "rogue"
+    elsif user_class_input == "5"
+       hero_variable = "hunter"
+    elsif user_class_input == "6"
+       hero_variable = "warlock"
+    elsif user_class_input == "7"
+       hero_variable = "priest"
+    elsif user_class_input == "8"
+       hero_variable = "shaman"
+    elsif user_class_input == "9"
+       hero_variable = "warrior"
+    else 
+      puts "try again!"
     end
+
+   @card_array.each do |card_element|
+     if card_element["playerClass"].downcase == hero_variable
+         @class_card_array << card_element["name"]
+     end
+  end
+end
+
+
+  def show_class_card_array
+    puts @class_card_array
   end
 
-def show_user_card_names
- @user_cards.each do |element|
-  puts element["name"]
-end
-end 
-
-
-
+# def show_user_card_names
+#  @user_cards.each do |element|
+#   puts element["name"]
+# end
+# end 
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,14 +1,39 @@
-require_relative "../wrapper.rb"
+# require_relative "../wrapper.rb"
 require_relative "../API.rb"
+require 'pry'
 
 new_user = User.new
 
-puts "Please type your class!"
+puts "Which Hearthstone Mood Card are you?"
 
-user_input = gets.chomp
+ puts "Press the number for threat that you like the most!"
 
-new_user.get_class_cards(user_input)
+ puts "1. My magic will tear you apart!" # - mage
+ puts "2. Justice demands retribution!" # - paladin
+ puts "3. Nature will rise against you!" # - druid
+ puts "4. I will be your death!"  # - rogue
+ puts "5. I will hunt you down!"  # - hunter 
+ puts "6. Your soul shall suffer!" # - warlock
+ puts "7. The Light shall burn you!" # - priest
+ puts "8. The Elements will destroy you!" # - shaman
+ puts "9. I will crush you!" # - warrior
 
-new_user.show_user_card_names
+
+user_hero_input = gets.chomp
+
+new_user.get_class_cards(user_hero_input)
+
+
+puts "Are you a thinker or a doer?"
+puts "1.thinker" # - spell
+puts "2. doer" # - minion
+
+
+
+ # puts "are you a thinker or a doer?"
+ # 
+
+
+
 
 
